0c4ff150d46f68663a1983fda712d22ff9ba736f
John Transue <johnktransue@gmail.com>
2021-02-22 10:50:01 -0800

    cors fixed

--- app.js
+++ app.js
@@ -63,14 +63,6 @@ app.use(
   cors()
 );
 
-app.get('/names/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 const index = require("./routes/index");
 app.use("/", index);
 
